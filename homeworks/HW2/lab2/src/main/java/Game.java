64740efca3e21ade3405baec0e49d7ee100ddaba
mixonij <mixon1000@mail.ru>
2019-10-24 19:22:08 +0300

    Добавлена игра быки и коровы

---
+++ homeworks/HW2/lab2/src/main/java/Game.java
@@ -0,0 +1,89 @@
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+
+    private static final Random random = new Random();
+    private List<String> words;
+    public String word;
+
+    public Game(List<String> words) {
+        this.words = words;
+    }
+
+    private String chooseSecretWord() {
+        return words.get(random.nextInt(words.size()));
+    }
+
+    public void SetSecretWord (String word){
+        this.word = word;
+    }
+
+    /**
+     * Вычисляем количество быков
+     * */
+    public int computeBulls(String guess) {
+        int bulls = 0;
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.charAt(i) == word.charAt(i)) {
+                bulls++;
+            }
+        }
+        return bulls;
+    }
+
+    /**
+     * Вычисляем количество коров
+     * */
+    private int computeCows(String guess) {
+        int cows = 0;
+        for (int i = 0; i < guess.length(); i++) {
+            int pos = word.indexOf(guess.charAt(i));
+            if (pos != -1 && pos != i) {
+                cows++;
+            }
+        }
+        return cows;
+    }
+
+    /**
+     * Старт
+     */
+    public void start() {
+        System.out.println("Hello!");
+        String playAgain = "Yes";
+        Scanner scanner = new Scanner(System.in);
+        do {
+            word = chooseSecretWord();
+            System.out.println("I offered a " + word.length() + "-letter word, your guess?");
+            playRound(scanner);
+            System.out.println("Wanna play again? Yes/No");
+            playAgain = scanner.next();
+        } while (playAgain.equalsIgnoreCase("Yes"));
+    }
+
+    /**
+     * Играть
+     */
+    private void playRound(Scanner scanner) {
+        int losses = 0;
+        while (losses < 15) {
+            String guess = scanner.next();
+            if (guess.length() != word.length()) {
+                System.out.println("Wrong length guess. Try again.");
+                continue;
+            }
+            if (guess.equals(word)) {
+                System.out.println("You won!");
+                return;
+            }
+            System.out.println("Bulls: " + computeBulls(guess));
+            System.out.println("Cows: " + computeCows(guess));
+            losses++;
+        }
+        System.out.println("You lose :(");
+    }
+
+
+}
---
+++ homeworks/HW2/lab2/src/test/java/AppTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void getWords() {
+        List<String> words = App.getWords("../dictionary.txt");
+        Assert.assertEquals(52976, words.size());
+    }
+
+    @Test
+    public void computeCows() {
+        Game game = new Game(null);
+        game.SetSecretWord("aaaa");
+        int result = game.computeBulls("aaaa");
+        Assert.assertEquals(4, result);
+    }
+}
